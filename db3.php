5094c258a9e9604ab4e77efc88eed343e5432bcf
Nuq00 <numanbasyir4@gmail.com>
2023-07-26 17:08:37 +0800

    Update db3.php

--- db3.php
+++ db3.php
@@ -1,20 +1,20 @@
 <?php 
 
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $dbname = "bipolar";
-
-// $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 $servername = "localhost";
-$username = "id20472835_bipolars";
-$password = "os?Hh9Uz06UlJg";
-$dbname = "id20472835_bipolar";
+$username = "root";
+$password = "";
+$dbname = "bipolar";
 
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// $servername = "localhost";
+// $username = "id20472835_bipolars";
+// $password = "os?Hh9Uz06UlJg";
+// $dbname = "id20472835_bipolar";
+
+// $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // $servername = "lrgs.ftsm.ukm.my";
 // $username = "a179430";
